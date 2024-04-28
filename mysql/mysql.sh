12393ad1f5c4c62d59cfbd97cfe2189bfdfbe4ce
Nguyen Quang Huy <trannguyenhan01092000@gmail.com>
2024-04-28 09:23:39 +0700

    add spline

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -2,9 +2,9 @@ docker run \
 --detach \
 --name=mysql_docker \
 --env="MYSQL_ROOT_PASSWORD=mysql12345" \
---publish 3306:3306 \
+--publish 9906:3306 \
 --volume=/home/trannguyenhan/app/mysql/conf.d:/etc/mysql/conf.d \
 --volume=/home/trannguyenhan/app/mysql/lib/mysql:/var/lib/mysql \
 --volume=/home/trannguyenhan/app/mysql/db:/docker-entrypoint-initdb.d \
 --restart always \
-mysql
+mysql:8.0-debian
